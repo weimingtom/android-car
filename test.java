8112a678aae30edcd1aeb2c9bee82ccd33004ddf
BadBoy.Tao@gmail.com <BadBoy.Tao@gmail.com@6b588903-8f1c-9ea5-c8ae-c7a1411f6e4a>
2011-04-17 14:22:55 +0000

    我前几天公司里太忙了，今天编译了一下，修改了一点，还有4个错误(打问号的地方)，你看看，是什么原因

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 package com.demo.android.test;
-
+//我前几天公司里太忙了，今天编译了一下，修改了一点，还有4个错误，你看看，是什么原因
 import android.app.Activity;
 import android.content.IntentFilter;
 import android.os.Bundle;
@@ -10,121 +10,94 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.View.OnTouchListener;
+import at.abraxas.amarino.Amarino;//需要这个语句 
 
-public class test extends Activity {
-	 private static final String DEVICE_ADDRESS =  "00:11:33:16:00:02";
-			private GestureDetector detector_up;
-			private GestureDetector detector_down;
-			private GestureDetector detector_left;
-			private GestureDetector detector_right;
-	
-		  private Button button1;
-		  private Button button2;
-		  private Button button3;
-		  private Button button4;
-	        
-	    /** Called when the activity is first created. */
-	    @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.main);
-	        
-	        Amarino.connect(this, DEVICE_ADDRESS);
-	        
-	        button1 = (Button)findViewById(R.id.up);
-	        button2 = (Button)findViewById(R.id.down);
-	        button3 = (Button)findViewById(R.id.left);
-	        button4 = (Button)findViewById(R.id.right);
-			// this is how you tell Amarino to connect to a specific BT device from within your own code
-	
-			    detector_up = new GestureDetector(onGestureListener_UP);
-			    detector_down = new GestureDetector(onGestureListener_DOWN);
-			    detector_left = new GestureDetector(onGestureListener_LEFT);
-			    detector_right = new GestureDetector(onGestureListener_RIGHT);
-				  button1.setOnTouchListener(onTouchListener);
-			    button2.setOnTouchListener(onTouchListener);
-			    button3.setOnTouchListener(onTouchListener);
-			    button4.setOnTouchListener(onTouchListener);
-	}
-	
-	private OnTouchListener onTouchListener = new OnTouchListener() {
-		
-		@Override
-		public boolean onTouch(View v, MotionEvent event) {
-			// TODO Auto-generated method stub
-			switch (v.getId()) {
-			case R.id.up:
-				return detector_up.onTouchEvent(event);
-				break;
-			case R.id.down:
-				return detector_down.onTouchEvent(event);
-				break;
-			case R.id.left:
-				return detector_left.onTouchEvent(event);
-				break;
-			case R.id.down:
-				return detector_right.onTouchEvent(event);
-				break;
-			default:
-				break;
-			}
-			return false;
-		}
-	};
-	private OnGestureListener onGestureListener_UP = new OnGestureListener() {
+public class Test extends Activity {
+         private static final String DEVICE_ADDRESS =  "00:11:33:16:00:02";
 
-		@Override
-		public void onLongPress(MotionEvent e) {
-			// TODO Auto-generated method stub
-			Amarino.sendDataToArduino(this, DEVICE_ADDRESS, 'a', 1);
-		}
-	};
-	private OnGestureListener onGestureListener_DOWN = new OnGestureListener() {
+                        private GestureDetector detector_left;
+                        private GestureDetector detector_right;
+        
+    
+                  private Button left;
+                  private Button right;
+                
+            /** Called when the activity is first created. */
+            @Override
+            public void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.main);
+                
+                Amarino.connect(this, DEVICE_ADDRESS);
+                
 
-		@Override
-		public void onLongPress(MotionEvent e) {
-			// TODO Auto-generated method stub
-			Amarino.sendDataToArduino(this, DEVICE_ADDRESS, 'b', 2);
-		}
-	};
-	private OnGestureListener onGestureListener_LEFT = new OnGestureListener() {
+                left = (Button)findViewById(R.id.left);
+                right = (Button)findViewById(R.id.right);
+                        // this is how you tell Amarino to connect to a specific BT device from within your own code
+        
 
-		@Override
-		public void onLongPress(MotionEvent e) {
-			// TODO Auto-generated method stub
-			Amarino.sendDataToArduino(this, DEVICE_ADDRESS, 'c', 3);
-		}
-	};
-	private OnGestureListener onGestureListener_RIGHT = new OnGestureListener() {
+                            detector_left = new GestureDetector(onGestureListener_LEFT);
+                            detector_right = new GestureDetector(onGestureListener_RIGHT);
 
-		@Override
-		public void onLongPress(MotionEvent e) {
-			// TODO Auto-generated method stub
-			Amarino.sendDataToArduino(this, DEVICE_ADDRESS, 'd', 4);
-		}
-	};
-	
+                            left.setOnTouchListener(onTouchListener);
+                            right.setOnTouchListener(onTouchListener);
+        }
+        
+        private OnTouchListener onTouchListener = new OnTouchListener() {
+                
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                        // TODO Auto-generated method stub
+                        switch (v.getId()) {
 
-	    @Override
-		protected void onStart() {
-	    	super.onStart();
-			// in order to receive broadcasted intents we need to register our receiver
-			registerReceiver(arduinoReceiver, new IntentFilter(AmarinoIntent.ACTION_RECEIVED));
-			
-			// this is how you tell Amarino to connect to a specific BT device from within your own code
-			Amarino.connect(this, DEVICE_ADDRESS);
-		}
+                        case R.id.left:
+                                return detector_left.onTouchEvent(event);
+                                break;
+                        case R.id.right:
+                                return detector_right.onTouchEvent(event);
+                                break;
+                        default:
+                                break;
+                        }
+                        return false;
+                }
+        };
+    
+        private OnGestureListener onGestureListener_LEFT = new OnGestureListener() {////OnGestureListener  error  ？
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                        // TODO Auto-generated method stub
+                        Amarino.sendDataToArduino(this, DEVICE_ADDRESS, 'c', 3);  ////sendDataToArduino error  ？
+                }
+        };
+        private OnGestureListener onGestureListener_RIGHT = new OnGestureListener() {  //OnGestureListener  error  ？
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                        // TODO Auto-generated method stub
+                	Amarino.sendDataToArduino(this, DEVICE_ADDRESS, 'd', 4);  //sendDataToArduino error  ？
+                }
+        };
+        
+
+            @Override
+                protected void onStart() {
+                super.onStart();
+                  
+                        // this is how you tell Amarino to connect to a specific BT device from within your own code
+                        Amarino.connect(this, DEVICE_ADDRESS);
+                }
 
 
-		@Override
-		protected void onStop() {
-			super.onStop();
-			
-			// if you connect in onStart() you must not forget to disconnect when your app is closed
-			Amarino.disconnect(this, DEVICE_ADDRESS);
-			
-			// do never forget to unregister a registered receiver
-			unregisterReceiver(arduinoReceiver);
-		}
+                @Override
+                protected void onStop() {
+                        super.onStop();
+                        
+                        // if you connect in onStart() you must not forget to disconnect when your app is closed
+                        Amarino.disconnect(this, DEVICE_ADDRESS);
+                        
+
+                }
 }    
-   
+ 
